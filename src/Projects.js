090601abed7151744b35643aa1e97403b8ff208d
Rayan Khan <rayan.ahkhan@gmail.com>
2022-12-26 21:01:33 +0300

    react set up and basic structure of app

---
+++ src/Projects.js
@@ -0,0 +1,9 @@
+const Projects = () => {
+    return (
+        <div className="Projects">
+            <h1>Projects</h1>
+        </div>
+    );
+}
+ 
+export default Projects;
---
+++ src/Resume.js
@@ -0,0 +1,9 @@
+const Resume = () => {
+    return (
+        <div className="Resume">
+            <h1>Resume</h1>
+        </div>
+    );
+}
+ 
+export default Resume;
