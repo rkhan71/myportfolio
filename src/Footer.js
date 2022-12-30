482f03d7c6fdba5b4da2332ad58a42d41e09912d
Rayan Khan <rayan.ahkhan@gmail.com>
2022-12-30 16:36:31 +0300

    added content to footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,15 +1,29 @@
+import logo from "./logo.png";
+import { Link } from "react-router-dom";
+import { Linkedin, TelephoneFill, EnvelopeAtFill, Github, CCircle } from "react-bootstrap-icons";
+
 const Footer = () => {
     return (
         <div className="Footer container-fluid border-top border-5 border-dark mt-auto">
-            <div className="row">
-                <div className="col">
-                    col 1
+            <div className="row h-100">
+                <div className="col d-flex flex-column align-items-center justify-content-center">
+                    <img className="img-fluid w-50" src={logo} alt="logo"></img>
+                    <p className="mt-3">Last Updated: 1st January 2023</p>
+                    <p>Copyright <CCircle /> 2023</p>
                 </div>
-                <div className="col">
-                    col 2
+                <div className="col d-flex flex-column align-items-center justify-content-center">
+                    <p className="fw-bold">Site Map</p>
+                    <Link to="/">Home</Link>
+                    <Link to="/projects">Projects</Link>
+                    <Link to="/resume">Resume</Link>
+                    <Link to="/contact">Contact</Link>
                 </div>
-                <div className="col">
-                    col 3
+                <div className="col d-flex flex-column align-items-center justify-content-center">
+                    <p className="fw-bold">Contact Information</p>
+                    <Link to="#"><EnvelopeAtFill /> rayan.ahkhan@gmail.com</Link>
+                    <Link to="#"><TelephoneFill /> 678-997-0922</Link>
+                    <a href="https://www.linkedin.com/in/khanrayan/"><Linkedin /> LinkedIn</a>
+                    <a href="https://github.com/rkhan71"><Github /> GitHub</a>
                 </div>
             </div>
         </div>
