9fe0f5f450e56f990f19f172feed420f40e4ea6c
Rayan Khan <rayan.ahkhan@gmail.com>
2022-12-30 18:56:40 +0300

    adds Web3Forms API functionality to contact form

--- src/Contact.js
+++ src/Contact.js
@@ -1,35 +1,70 @@
 import { useState } from "react";
+import Loading from "./Loading";
+import Error from "./Error";
 
 // Page where user can contact me through a form that sends me an email
 const Contact = () => {
-    // Using useState hook to track users' form inputs
-    const [name, setName] = useState("");
-    const [email, setEmail] = useState("");
-    const [message, setMessage] = useState("");
+    // Using useState hook to update status of form
+    const [sent, setSent] = useState(false);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(false);
+
+    // Using Web3Forms API to get information from contact form to my email
+    const handleSubmit = async (event) => {
+        setLoading(true);
+        event.preventDefault();
+        const formData = new FormData(event.target);
+    
+        formData.append("access_key", null);
+    
+        const res = await fetch("https://api.web3forms.com/submit", {
+            method: "POST",
+            body: formData
+        }).then((res) => res.json());
+    
+        if (res.success) {
+            setLoading(false);
+            setSent(true);
+        } else {
+            setLoading(false);
+            setError("Something went wrong");
+        }
+    };
 
     return (
+        // Conditional templates to display form, loading symbol when message is sending,
+        // sent message when message is sent, and error template when error occurs
         <div className="Contact mb-5">
             <div className="container-fluid d-flex justify-content-center align-items-center flex-column heading">
                 <h1>Rayan's Online Portfolio</h1>
                 <button className="btn mt-4 fw-bold">Contact</button>
             </div>
-            <div className="form container">
-                <div className="mb-3">
-                    <label htmlFor="name" className="form-label">Name</label>
-                    <input type="text" className="form-control" id="name" placeholder="Rayan" onChange={e => setName(e.target.value)}/>
+            {loading && <Loading />}
+            {error && <Error error={ error } />}
+            {!(sent) && !(loading) && !(error) && (
+                <form className="form container" onSubmit={ handleSubmit }>
+                    <div className="mb-3">
+                        <label htmlFor="name" className="form-label">Name</label>
+                        <input type="text" className="form-control" id="name" name="name" placeholder="Rayan" />
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="email" className="form-label">Email Address</label>
+                        <input type="email" className="form-control" id="email" name="email" placeholder="name@example.com" />
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="message" className="form-label">Message</label>
+                        <textarea className="form-control" id="message" name="message"></textarea>
+                    </div>
+                    <div className="text-center">
+                        <button type="submit" className="btn fw-bold">Submit</button>
+                    </div>
+                </form>
+            )}
+            {sent && (
+                <div className="container text-center">
+                    Message sent successfuly!
                 </div>
-                <div className="mb-3">
-                    <label htmlFor="email" className="form-label">Email Address</label>
-                    <input type="email" className="form-control" id="email" placeholder="name@example.com" onChange={e => setEmail(e.target.value)}/>
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="message" className="form-label">Message</label>
-                    <textarea className="form-control" id="message" onChange={e => setMessage(e.target.value)}></textarea>
-                </div>
-                <div className="text-center">
-                    <button type="submit" className="btn fw-bold">Submit</button>
-                </div>
-            </div>
+            )}
         </div>
     );
 }
--- src/Error.js
+++ src/Error.js
@@ -1,8 +1,10 @@
 // Template to display error message
 const Error = ({ error }) => {
     return (
-        <div className="container-fluid d-flex justify-content-center align-items-center">
+        // Error message with button to refresh page
+        <div className="Error container-fluid d-flex justify-content-center align-items-center flex-column">
             <h1>{ error }</h1>
+            <button className="btn fw-bold mt-3" onClick={() => window.location.reload(false)}>Refresh Page</button>
         </div>
     );
 }
